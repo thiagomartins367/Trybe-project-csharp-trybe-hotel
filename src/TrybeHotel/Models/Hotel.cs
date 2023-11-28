b105f71bdd4a898fdd6c6682c9c5b6cf2c0fea75
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-28 18:33:57 -0300

    feat(Hotel.cs): Model "Hotel"

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -3,6 +3,12 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
-public class Hotel {
-
+public class Hotel
+{
+    public int HotelId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Address { get; set; } = null!;
+    public int CityId { get; set; }
+    public City? City { get; set; }
+    public ICollection<Room>? Rooms { get; set; }
 }
