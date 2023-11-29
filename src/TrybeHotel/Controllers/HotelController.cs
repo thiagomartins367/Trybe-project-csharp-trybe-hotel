37f831b095d35d344e6ebd2f09e90e4c40d4911f
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-29 18:05:59 -0300

    feat(HotelController.cs): Método "GetHotels"
    Método da controller "HotelController"

--- src/TrybeHotel/Controllers/HotelController.cs
+++ src/TrybeHotel/Controllers/HotelController.cs
@@ -18,8 +18,10 @@ namespace TrybeHotel.Controllers
         
         // 4. Desenvolva o endpoint GET /hotel
         [HttpGet]
-        public IActionResult GetHotels(){
-            throw new NotImplementedException();
+        public IActionResult GetHotels()
+        {
+            var hotels = _repository.GetHotels();
+            return Ok(hotels);
         }
 
         // 5. Desenvolva o endpoint POST /hotel
