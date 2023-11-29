b7724044691b3235c71ffa8cc4ddd19737ca8a8d
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-29 18:01:07 -0300

    feat(HotelDto.cs): DTO "HotelDto"
    Objeto de transferência de dados "HotelDto"

--- src/TrybeHotel/Dto/HotelDto.cs
+++ src/TrybeHotel/Dto/HotelDto.cs
@@ -1,5 +1,9 @@
 namespace TrybeHotel.Dto {
-     public class HotelDto {
-
+    public class HotelDto {
+        public int HotelId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public int CityId { get; set; }
+        public string CityName { get; set; } = null!;
     }
 }
