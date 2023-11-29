a8deb92b21bdb52b9ec646036f87b321311304d3
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-29 16:47:00 -0300

    feat(CityRepository.cs): Método "GetCities"
    Método do repository "CityRepository"

--- src/TrybeHotel/Repository/CityRepository.cs
+++ src/TrybeHotel/Repository/CityRepository.cs
@@ -14,7 +14,9 @@ namespace TrybeHotel.Repository
         // 2. Desenvolva o endpoint GET /city
         public IEnumerable<CityDto> GetCities()
         {
-            throw new NotImplementedException();
+            return _context.Cities.Select(
+                city => new CityDto() { CityId = city.CityId, Name = city.Name }
+            );
         }
 
         // 3. Desenvolva o endpoint POST /city
