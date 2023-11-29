acaf252837e4623dc7519b80ea20557e446b0ad4
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-29 17:17:12 -0300

    feat(CityRepository.cs): Método "AddCity"
    Método da repository "CityRepository"

--- src/TrybeHotel/Repository/CityRepository.cs
+++ src/TrybeHotel/Repository/CityRepository.cs
@@ -22,7 +22,9 @@ namespace TrybeHotel.Repository
         // 3. Desenvolva o endpoint POST /city
         public CityDto AddCity(City city)
         {
-            throw new NotImplementedException();
+            _context.Cities.Add(city);
+            _context.SaveChanges();
+            return new CityDto() { CityId = city.CityId, Name = city.Name };
         }
 
     }
