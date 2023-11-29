c507f78e34ecda43e938e7b32b81f1b17d327bcb
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-29 17:18:04 -0300

    feat(CityController.cs): Método "PostCity"
    Método da controller "CityController"

--- src/TrybeHotel/Controllers/CityController.cs
+++ src/TrybeHotel/Controllers/CityController.cs
@@ -24,8 +24,10 @@ namespace TrybeHotel.Controllers
 
         // 3. Desenvolva o endpoint POST /city
         [HttpPost]
-        public IActionResult PostCity([FromBody] City city){
-            throw new NotImplementedException();
+        public IActionResult PostCity([FromBody] City city)
+        {
+            var newCity = _repository.AddCity(city);
+            return Created("", newCity);
         }
     }
 }
