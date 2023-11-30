6792cb62259d2ec84603bb8fdc618cd935f06dfa
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-11-30 19:17:52 -0300

    style(ITrybeHotelContext.cs): :art: Formatação de código

--- src/TrybeHotel/Repository/ITrybeHotelContext.cs
+++ src/TrybeHotel/Repository/ITrybeHotelContext.cs
@@ -3,7 +3,7 @@ using TrybeHotel.Models;
 
 namespace TrybeHotel.Repository
 {
-    public interface ITrybeHotelContext 
+    public interface ITrybeHotelContext
     {
         public DbSet<City> Cities { get; set; }
         public DbSet<Hotel> Hotels { get; set; }
