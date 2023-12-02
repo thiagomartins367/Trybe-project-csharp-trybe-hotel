3463c977e6dab8ec7e02f57680e5bd16f2030d63
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-01 22:46:42 -0300

    refactor(Hotel.cs): :recycle: DataAnnotations de "ComponentModel" adicionadas
    DataAnnotations "Key" e "ForeignKey" adicionadas para especificar chave primária e estrangeira da model respectivamente

--- src/TrybeHotel/Models/Hotel.cs
+++ src/TrybeHotel/Models/Hotel.cs
@@ -5,9 +5,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 // 1. Implemente as models da aplicação
 public class Hotel
 {
+    [Key]
     public int HotelId { get; set; }
     public string Name { get; set; } = null!;
     public string Address { get; set; } = null!;
+
+    [ForeignKey("CityId")]
     public int CityId { get; set; }
     public City? City { get; set; }
     public ICollection<Room>? Rooms { get; set; }
