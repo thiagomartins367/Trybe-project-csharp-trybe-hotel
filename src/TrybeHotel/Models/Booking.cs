0de11c90be2e684bf1b0fcc26298082618fcd727
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-06 19:45:00 -0300

    feat(Booking.cs): Model "Booking"

--- src/TrybeHotel/Models/Booking.cs
+++ src/TrybeHotel/Models/Booking.cs
@@ -2,7 +2,20 @@ namespace TrybeHotel.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
- // 1. Implemente as models da aplicação
-public class Booking {
+// 1. Implemente as models da aplicação
+public class Booking
+{
+    [Key]
+    public int BookingId { get; set; }
+    public DateTime CheckIn { get; set; }
+    public DateTime CheckOut { get; set; }
+    public int GuestQuant { get; set; }
 
+    [ForeignKey("UserId")]
+    public int UserId { get; set; }
+    public User? User { get; set; }
+
+    [ForeignKey("RoomId")]
+    public int RoomId { get; set; }
+    public Room? Room { get; set; }
 }
