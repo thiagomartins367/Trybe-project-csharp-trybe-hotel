839f83988a39b262f7a37478d49caad50030cc5c
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-06 19:47:52 -0300

    feat(TrybeHotelContext.cs): Tabelas "Users" e "Bookings"

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -7,6 +7,8 @@ public class TrybeHotelContext : DbContext, ITrybeHotelContext
     public DbSet<City> Cities { get; set; }
     public DbSet<Hotel> Hotels { get; set; }
     public DbSet<Room> Rooms { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
 
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
     {
