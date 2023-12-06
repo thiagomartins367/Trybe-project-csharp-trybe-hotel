b1f01adbbad80c08d589e243094ec76c0199144d
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-06 19:44:30 -0300

    feat(User.cs): Model "User"

--- src/TrybeHotel/Models/User.cs
+++ src/TrybeHotel/Models/User.cs
@@ -3,6 +3,13 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 // 1. Implemente as models da aplicação
-public class User {
-
+public class User
+{
+    [Key]
+    public int UserId { get; set; }
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string UserType { get; set; } = null!;
+    public ICollection<Booking>? Bookings { get; set; }
 }
