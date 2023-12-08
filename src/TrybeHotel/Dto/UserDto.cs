8bbfdcbee53ba5c81fdb71a18451998173f0b76a
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-08 15:56:51 -0300

    feat(UserDto.cs): DTO "LoginDto"

--- src/TrybeHotel/Dto/UserDto.cs
+++ src/TrybeHotel/Dto/UserDto.cs
@@ -15,7 +15,9 @@ namespace TrybeHotel.Dto
         public string Password { get; set; } = null!;
     }
 
-    public class LoginDto {
-        
+    public class LoginDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
     }
 }
