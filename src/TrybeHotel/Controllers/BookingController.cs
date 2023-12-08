5c96707f4efa880b2b3eb81e926980adb6ad73b5
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-08 20:12:26 -0300

    feat(BookingController.cs): Método de rota "Add"
    Responsável pelo controle de requsições/respostas da rota "POST /booking"

--- src/TrybeHotel/Controllers/BookingController.cs
+++ src/TrybeHotel/Controllers/BookingController.cs
@@ -11,24 +11,35 @@ namespace TrybeHotel.Controllers
 {
     [ApiController]
     [Route("booking")]
-  
     public class BookingController : Controller
     {
         private readonly IBookingRepository _repository;
+
         public BookingController(IBookingRepository repository)
         {
             _repository = repository;
         }
 
         [HttpPost]
-        public IActionResult Add([FromBody] BookingDtoInsert bookingInsert){
-            throw new NotImplementedException();
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [Authorize(Policy = "Client")]
+        public IActionResult Add([FromBody] BookingDtoInsert bookingInsert)
+        {
+            Room room = _repository.GetRoomById(bookingInsert.RoomId);
+            if (room.RoomId == 0) return NotFound(new { Message = "room not found" });
+            if (room.Capacity < bookingInsert.GuestQuant)
+                return BadRequest(new { Message = "Guest quantity over room capacity" });
+            var userTokenClaims = HttpContext.User.Identity as ClaimsIdentity;
+            string userEmail = userTokenClaims!.Claims
+                .First(claim => claim.Type == ClaimTypes.Email).Value;
+            BookingResponse newBooking = _repository.Add(bookingInsert, userEmail);
+            return Created("GetBooking", newBooking);
         }
 
-
         [HttpGet("{Bookingid}")]
-        public IActionResult GetBooking(int Bookingid){
-           throw new NotImplementedException();
+        public IActionResult GetBooking(int Bookingid)
+        {
+            throw new NotImplementedException();
         }
     }
 }
