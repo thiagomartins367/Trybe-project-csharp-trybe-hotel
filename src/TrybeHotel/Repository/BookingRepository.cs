7c820122aba2163eda8eb547dd9017d1dd2e501e
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-08 20:03:01 -0300

    feat(BookingRepository.cs): Métodos da repository "BookingRepository"
    Métodos "Add", "GetBooking", "GetRoomById" e "GetUserByEmail"

--- src/TrybeHotel/Repository/BookingRepository.cs
+++ src/TrybeHotel/Repository/BookingRepository.cs
@@ -1,11 +1,13 @@
 using TrybeHotel.Models;
 using TrybeHotel.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace TrybeHotel.Repository
 {
     public class BookingRepository : IBookingRepository
     {
         protected readonly ITrybeHotelContext _context;
+
         public BookingRepository(ITrybeHotelContext context)
         {
             _context = context;
@@ -13,19 +15,64 @@ namespace TrybeHotel.Repository
 
         public BookingResponse Add(BookingDtoInsert booking, string email)
         {
-            throw new NotImplementedException();
+            User user = GetUserByEmail(email);
+            var bookingToInsert = new Booking()
+            {
+                CheckIn = booking.CheckIn,
+                CheckOut = booking.CheckOut,
+                GuestQuant = booking.GuestQuant,
+                UserId = user.UserId,
+                RoomId = booking.RoomId,
+            };
+            _context.Bookings.Add(bookingToInsert);
+            _context.SaveChanges();
+            return GetBooking(bookingToInsert.BookingId, email);
         }
 
         public BookingResponse GetBooking(int bookingId, string email)
         {
-            throw new NotImplementedException();
+            User user = GetUserByEmail(email);
+            var booking = _context.Bookings
+                .Include(b => b.Room)
+                .ThenInclude(room => room!.Hotel)
+                .ThenInclude(hotel => hotel!.City)
+                .FirstOrDefault(b => b.BookingId == bookingId && b.UserId == user.UserId);
+            if (booking is null) return new BookingResponse();
+            return new BookingResponse()
+            {
+                BookingId = booking.BookingId,
+                CheckIn = booking.CheckIn,
+                CheckOut = booking.CheckOut,
+                GuestQuant = booking.GuestQuant,
+                Room = new RoomDto()
+                {
+                    RoomId = booking.Room!.RoomId,
+                    Name = booking.Room.Name,
+                    Capacity = booking.Room.Capacity,
+                    Image = booking.Room.Image,
+                    Hotel = new HotelDto()
+                    {
+                        HotelId = booking.Room!.Hotel!.HotelId,
+                        Name = booking.Room!.Hotel!.Name,
+                        Address = booking.Room!.Hotel!.Address,
+                        CityId = booking.Room!.Hotel!.City!.CityId,
+                        CityName = booking.Room!.Hotel!.City!.Name,
+                    }
+                },
+            };
         }
 
         public Room GetRoomById(int RoomId)
         {
-            throw new NotImplementedException();
+            var room = _context.Rooms.FirstOrDefault(r => r.RoomId == RoomId);
+            if (room is null) return new Room();
+            return room;
         }
 
+        private User GetUserByEmail(string userEmail)
+        {
+            var user = _context.Users.First(user => user.Email == userEmail);
+            return user;
+        }
     }
-
 }
