2ee3705d4246df1c4c41477a11328e2cbb92669a
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-08 15:17:25 -0300

    feat(UserDto.cs): DTOs "UserDto" e "UserDtoInsert"

--- src/TrybeHotel/Dto/UserDto.cs
+++ src/TrybeHotel/Dto/UserDto.cs
@@ -1,14 +1,21 @@
 namespace TrybeHotel.Dto
 {
-    public class UserDto {
-
+    public class UserDto
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string UserType { get; set; } = null!;
     }
 
-    public class UserDtoInsert {
-       
+    public class UserDtoInsert
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
     }
 
     public class LoginDto {
-      
+        
     }
 }
