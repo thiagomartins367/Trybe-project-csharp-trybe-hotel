04c248927be1067c68995afa3d8166db2f378be0
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-12-08 15:20:12 -0300

    feat(UserRepository.cs): Métodos "Add", "GetUserByEmail", "CopyEqualPropertiesFromUser" e "FindAndCopyProperties"
    Métodos auxiliares privados "CopyEqualPropertiesFromUser" e "FindAndCopyProperties"

--- src/TrybeHotel/Repository/UserRepository.cs
+++ src/TrybeHotel/Repository/UserRepository.cs
@@ -1,15 +1,19 @@
 using TrybeHotel.Models;
 using TrybeHotel.Dto;
+using System.ComponentModel;
+using System.Text.Json;
 
 namespace TrybeHotel.Repository
 {
     public class UserRepository : IUserRepository
     {
         protected readonly ITrybeHotelContext _context;
+
         public UserRepository(ITrybeHotelContext context)
         {
             _context = context;
         }
+
         public UserDto GetUserById(int userId)
         {
             throw new NotImplementedException();
@@ -17,22 +21,76 @@ namespace TrybeHotel.Repository
 
         public UserDto Login(LoginDto login)
         {
-           throw new NotImplementedException();
+            throw new NotImplementedException();
         }
+
         public UserDto Add(UserDtoInsert user)
         {
-            throw new NotImplementedException(); 
+            var newUser = new User()
+            {
+                Name = user.Name,
+                Email = user.Email,
+                Password = user.Password,
+                UserType = "client",
+            };
+            _context.Users.Add(newUser);
+            _context.SaveChanges();
+            return CopyEqualPropertiesFromUser(newUser, new UserDto(), new { });
         }
 
         public UserDto GetUserByEmail(string userEmail)
         {
-            throw new NotImplementedException();
+            var user = _context.Users.FirstOrDefault(user => user.Email == userEmail);
+            if (user is null)
+                return CopyEqualPropertiesFromUser(
+                    new User() { Email = "" },
+                    new UserDto(),
+                    null
+                );
+            return CopyEqualPropertiesFromUser(user, new UserDto(), null);
         }
 
         public IEnumerable<UserDto> GetUsers()
         {
-           throw new NotImplementedException();
+            throw new NotImplementedException();
         }
 
+        private static T CopyEqualPropertiesFromUser<T>(
+            User user,
+            T expectedObject,
+            object? defaultValues
+        )
+        {
+            if (expectedObject is null) return expectedObject;
+            var expectedObjectCopied = FindAndCopyProperties(user, expectedObject, null);
+            expectedObjectCopied = FindAndCopyProperties(
+                user,
+                expectedObjectCopied,
+                defaultValues
+            );
+            return expectedObjectCopied;
+        }
+
+        private static T FindAndCopyProperties<T>(
+            User user,
+            T expectedObject,
+            object? defaultValues
+        )
+        {
+            var objectToIterate = defaultValues is null ? expectedObject : defaultValues;
+            if (objectToIterate is null) return expectedObject;
+            var userProperties = user.GetType().GetProperties();
+            foreach (var property in objectToIterate.GetType().GetProperties())
+            {
+                var userProperty = userProperties
+                    .FirstOrDefault(
+                        propertyInfo => propertyInfo.Name == property.Name
+                        && propertyInfo.GetType() == property.GetType()
+                    );
+                if (userProperty is not null)
+                    property.SetValue(expectedObject, userProperty.GetValue(user));
+            }
+            return expectedObject;
+        }
     }
 }
