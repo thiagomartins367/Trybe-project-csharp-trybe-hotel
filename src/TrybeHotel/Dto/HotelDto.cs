df741516404ef4fd83f738c918373d15b43119d0
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-01-09 19:10:34 -0300

    refactor(HotelDto.cs): :recycle: Propriedade "State" adicionada

--- src/TrybeHotel/Dto/HotelDto.cs
+++ src/TrybeHotel/Dto/HotelDto.cs
@@ -7,5 +7,6 @@ namespace TrybeHotel.Dto
         public string Address { get; set; } = null!;
         public int CityId { get; set; }
         public string CityName { get; set; } = null!;
+        public string State { get; set; } = null!;
     }
 }
