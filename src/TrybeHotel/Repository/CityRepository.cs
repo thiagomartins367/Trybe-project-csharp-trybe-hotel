5b38c33aac56f8c751d4f414ffeeb6b2bdcac0e7
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-01-09 16:57:21 -0300

    feat(CityRepository.cs): Método "UpdateCity"

--- src/TrybeHotel/Repository/CityRepository.cs
+++ src/TrybeHotel/Repository/CityRepository.cs
@@ -31,7 +31,9 @@ namespace TrybeHotel.Repository
         // 3. Desenvolva o endpoint PUT /city
         public CityDto UpdateCity(City city)
         {
-            throw new NotImplementedException();
+            _context.Cities.Update(city);
+            _context.SaveChanges();
+            return new CityDto() { CityId = city.CityId, Name = city.Name, State = city.State };
         }
     }
 }
