dd455fcb2587a181154ae70f83e942f6543f1946
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-13 00:56:19 -0300

    feat(EnvConfig.cs): Classe "EnvConfig"
    Responsável por definir e verificar todas as variáveis de ambiente que a API utiliza através do método "Load"

---
+++ src/TrybeHotel/Env/EnvConfig.cs
@@ -0,0 +1,39 @@
+namespace TrybeHotel.Env;
+
+// Código desenvolvido baseado no artigo:
+// https://medium.com/xp-inc/dica-r%C3%A1pida-net-env-76b0c66dac4
+public static class EnvConfig
+{
+    public static void Load(string path)
+    {
+        if (!File.Exists(path))
+            throw new FileNotFoundException(
+                $"The \"{path}\" environment variables file not found!"
+            );
+
+        foreach (var line in File.ReadAllLines(path))
+        {
+            var part = line.Split(
+                '=',
+                StringSplitOptions.RemoveEmptyEntries);
+
+            if (part.Length != 2)
+                continue;
+
+            Environment.SetEnvironmentVariable(part[0], part[1]);
+        }
+
+        VerifyEnvironmentVariables();
+    }
+
+    private static void VerifyEnvironmentVariables()
+    {
+        string[] variableNames = EnvironmentVariables.GetVariableNames();
+        foreach (string name in variableNames)
+        {
+            var variableValue = Environment.GetEnvironmentVariable(name);
+            if (variableValue is null)
+                throw new Exception($"Environment variable \"{name}\" not found or is empty!");
+        }
+    }
+}
