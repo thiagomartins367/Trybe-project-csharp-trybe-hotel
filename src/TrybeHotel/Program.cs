0cd77aca75a361129310a3691362db431e402044
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-13 00:59:02 -0300

    feat(Program.cs): Carregar variáveis ​​de ambiente
    Define as variáveis de acordo com o tipo de ambiente (Production ou Development)

--- src/TrybeHotel/Program.cs
+++ src/TrybeHotel/Program.cs
@@ -4,12 +4,27 @@ using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using TrybeHotel.Models;
+using TrybeHotel.Env;
 using TrybeHotel.Services;
 using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var port = builder.Configuration["PORT"];
+// Load environment variables
+var rootPath = Directory.GetCurrentDirectory();
+string? dotenvPath = null;
+const string developmentDotenvFileName = ".env.development.local";
+const string productionDotenvFileName = ".env.production.local";
+if (builder.Environment.IsDevelopment())
+    dotenvPath = Path.Combine(rootPath, developmentDotenvFileName);
+else
+{
+    dotenvPath = Path.Combine(rootPath, productionDotenvFileName);
+}
+EnvConfig.Load(dotenvPath);
+
+// Configure port
+var port = Environment.GetEnvironmentVariable("PORT");
 builder.WebHost.UseUrls($"http://*:{port}");
 
 // Add services to the container.
