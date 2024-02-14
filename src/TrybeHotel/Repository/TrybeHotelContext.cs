ce4bf71907335a67365b1c41b88da99ce3e2c21b
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-14 15:55:25 -0300

    feat(TrybeHotelContext.cs): Dialeto do banco de dados
    Esse recurso é responsável por definir qual dialeto de banco de dados será usado pela API (SQL Server, MySQL, etc...)

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using TrybeHotel.Enums;
 using TrybeHotel.Env;
 using TrybeHotel.Models;
 
@@ -10,26 +11,67 @@ public class TrybeHotelContext : DbContext, ITrybeHotelContext
     public DbSet<Room> Rooms { get; set; } = null!;
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<Booking> Bookings { get; set; } = null!;
+    private DatabaseDialect DbDialect { get; set; } = DatabaseDialect.SQL_SERVER;
+    private string DbServer { get; set; } = Environment
+        .GetEnvironmentVariable(EnvironmentVariables.DB_SERVER)!;
+    private string DbPort { get; set; } = Environment
+        .GetEnvironmentVariable(EnvironmentVariables.DB_PORT)!;
+    private string DbName { get; set; } = Environment
+        .GetEnvironmentVariable(EnvironmentVariables.DB_NAME)!;
+    private string DbUser { get; set; } = Environment
+        .GetEnvironmentVariable(EnvironmentVariables.DB_USER)!;
+    private string DbPassword { get; set; } = Environment
+        .GetEnvironmentVariable(EnvironmentVariables.DB_PASSWORD)!;
 
-    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
-    public TrybeHotelContext() { }
+    public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options)
+    {
+        SetDatabaseDialect();
+    }
+    public TrybeHotelContext()
+    {
+        SetDatabaseDialect();
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        var connectionString = GetConnectionString();
-        optionsBuilder.UseSqlServer(connectionString);
+        string connectionString = GetConnectionString();
+        switch (DbDialect)
+        {
+            case DatabaseDialect.MYSQL:
+                optionsBuilder.UseMySql(
+                    connectionString, ServerVersion.AutoDetect(connectionString), null);
+                break;
+            default:
+                optionsBuilder.UseSqlServer(connectionString);
+                break;
+        }
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) { }
 
-    private static string GetConnectionString()
+    private string GetConnectionString()
     {
-        var dbServer = Environment.GetEnvironmentVariable(EnvironmentVariables.DB_SERVER);
-        var dbName = Environment.GetEnvironmentVariable(EnvironmentVariables.DB_NAME);
-        var dbUser = Environment.GetEnvironmentVariable(EnvironmentVariables.DB_USER);
-        var dbPassword = Environment.GetEnvironmentVariable(EnvironmentVariables.DB_PASSWORD);
-        var connectionStringFirstPart = $"Server={dbServer};Database={dbName}";
-        var connectionStringSecondPart = $"User={dbUser};Password={dbPassword}";
-        return $"{connectionStringFirstPart};{connectionStringSecondPart};TrustServerCertificate=True";
+        switch (DbDialect)
+        {
+            case DatabaseDialect.MYSQL:
+                var connectionStringMysql = $"Server={DbServer};Database={DbName}";
+                connectionStringMysql += string.Concat(";", $"User Id={DbUser};Password={DbPassword}");
+                connectionStringMysql += string.Concat(";", $"Port={DbPort}");
+                return connectionStringMysql;
+            default:
+                var connectionStringSqlServer = $"Server={DbServer},{DbPort};Database={DbName}";
+                connectionStringSqlServer += string.Concat(";", $"User={DbUser};Password={DbPassword}");
+                connectionStringSqlServer += string.Concat(";", "TrustServerCertificate=True");
+                return connectionStringSqlServer;
+        }
+    }
+
+    private void SetDatabaseDialect()
+    {
+        var envDbDialect = Environment.GetEnvironmentVariable(EnvironmentVariables.DB_DIALECT);
+        if (envDbDialect is null) return;
+        var envDbDialectIsInt = int.TryParse(envDbDialect, out int dbDialectValue);
+        if (!envDbDialectIsInt && !Enum.IsDefined(typeof(DatabaseDialect), dbDialectValue)) return;
+        DbDialect = (DatabaseDialect)dbDialectValue;
     }
 }
