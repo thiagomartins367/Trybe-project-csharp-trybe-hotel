4e206ffdc9dd260a81b1e80ab79be801384b0653
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-18 16:32:48 -0300

    feat(NotFoundException.cs): Classe "NotFoundException"
    Responsável por construir exceção de erro "Not Found" da API

---
+++ src/TrybeHotel/Errors/ApiExceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+namespace TrybeHotel.Errors.ApiExceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException() : base() { }
+    public NotFoundException(string? message) : base(message) { }
+    public NotFoundException(string? message, Exception innerException) : base(message, innerException) { }
+};
