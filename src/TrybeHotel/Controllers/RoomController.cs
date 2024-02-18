5f38e319c51d2bc96e9b92802c08c7f0825cf5d7
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-18 19:49:32 -0300

    docs(RoomController.cs): 📝 Informações das rotas "GET /room/{HotelId}" e "POST /room"
    Informações adicionadas por comentário na documentação do "Swagger"

--- src/TrybeHotel/Controllers/RoomController.cs
+++ src/TrybeHotel/Controllers/RoomController.cs
@@ -1,14 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
-using TrybeHotel.Models;
-using TrybeHotel.Repository;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using TrybeHotel.Models;
+using TrybeHotel.Repository;
+using TrybeHotel.Dto;
+using TrybeHotel.Errors;
 
 namespace TrybeHotel.Controllers
 {
     [ApiController]
     [Route("room")]
+    [Produces("application/json")]
     public class RoomController : Controller
     {
         private readonly IRoomRepository _repository;
@@ -18,6 +20,16 @@ namespace TrybeHotel.Controllers
             _repository = repository;
         }
 
+        /// <summary>
+        ///     Retorna todos os quartos de um hotel pelo id do hotel
+        /// </summary>
+        /// <response code="200">`OK` Retorna todos os quartos de um hotel pelo id do hotel</response>
+        /// <response code="400">`Bad Request` Retorna resposta padrão de erro de validação do parâmetro <i>HotelId</i></response>
+        /// <response code="404">`Not Found` Retorna mensagem <b>"Hotel not found"</b> <br /> <p>`Not Found` Acesso a um <i>endpoint</i> que não existe. (Sem corpo de resposta)</p></response>
+        /// <response code="405">`Method Not Allowed` Acesso incorreto ao <i>endpoint</i>. Ex: <b>/room/</b> (Sem corpo de resposta)</response>
+        [ProducesResponseType(typeof(IEnumerable<RoomDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status404NotFound)]
         [HttpGet("{HotelId}")]
         public IActionResult GetRoom(int HotelId)
         {
@@ -32,13 +44,45 @@ namespace TrybeHotel.Controllers
             }
         }
 
+        /// <summary>
+        ///     Adiciona um novo quarto
+        /// </summary>
+        /// <remarks>
+        /// ⚠️ Apenas usuários <b>administradores</b> podem consumir esse <i>endpoint</i>.
+        /// <br />
+        /// <br />
+        /// Request body example:
+        ///
+        ///     {
+        ///        "name": "Suite Deluxe",
+        ///        "capacity": 4,
+        ///        "image": "https://image-example.com/suite/suite-deluxe.jpg"
+        ///        "hotelId": 3
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">`Created` Retorna os dados do novo quarto</response>
+        /// <response code="400">`Bad Request` Retorna resposta padrão de erro de validação de campos</response>
+        /// <response code="401">`Unauthorized` Quando o token do usuário não é informado ou é inválido. (Sem corpo de resposta)</response>
+        /// <response code="403">`Forbidden` Violação da política de usuário <b>Admin</b> através do campo <i>role</i> do token do usuário. (Sem corpo de resposta)</response>
+        /// <response code="404">`Not Found` Acesso a um <i>endpoint</i> que não existe. (Sem corpo de resposta)</response>
+        /// <response code="415">`Unsupported Media Type` Retorna resposta padrão de tipo de mídia não suportado</response>
+        [ProducesResponseType(typeof(RoomDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
         [HttpPost]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [Authorize(Policy = "Admin")]
-        public IActionResult PostRoom([FromBody] Room room)
+        public IActionResult PostRoom([FromBody] RoomDtoInsert roomToInsert)
         {
-            var newRoom = _repository.AddRoom(room);
-            return Created("", newRoom);
+            var newRoom = _repository.AddRoom(new Room
+                {
+                    Name = roomToInsert.Name,
+                    Capacity = roomToInsert.Capacity,
+                    Image = roomToInsert.Image,
+                    HotelId = roomToInsert.HotelId,
+                });
+            return Created(string.Empty, newRoom);
         }
 
         [HttpDelete("{RoomId}")]
