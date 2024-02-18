63e3b4aa9e7be9bd98ec74b21e7c8920921377b0
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-18 19:47:34 -0300

    feat(RoomDto.cs): Classe DTO "RoomDtoInsert"
    Responsável por construir objeto com campos do request body em requisições para rota "/room"

--- src/TrybeHotel/Dto/RoomDto.cs
+++ src/TrybeHotel/Dto/RoomDto.cs
@@ -8,4 +8,12 @@ namespace TrybeHotel.Dto
         public string Image { get; set; } = null!;
         public HotelDto Hotel { get; set; } = null!;
     }
+
+    public class RoomDtoInsert
+    {
+        public string Name { get; set; } = null!;
+        public int Capacity { get; set; }
+        public string Image { get; set; } = null!;
+        public int HotelId { get; set; }
+    }
 }
