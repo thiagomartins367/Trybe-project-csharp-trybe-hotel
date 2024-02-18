10fb512ba78d9a4e3523bcdf51248e11e0bc5082
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-18 16:26:20 -0300

    docs(StatusController.cs): 📝 Informações da rota "GET /"
    Informações adicionadas por comentário na documentação do "Swagger"

--- src/TrybeHotel/Controllers/StatusController.cs
+++ src/TrybeHotel/Controllers/StatusController.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using TrybeHotel.Utils;
 
 namespace TrybeHotel.Controllers
 {
     [ApiController]
     [Route("/")]
+    [Produces("application/json")]
     public class StatusController : Controller
     {
+        /// <summary>
+        ///     Verifica o status da API
+        /// </summary>
+        /// <response code="200">`OK` Retorna mensagem <b>"online"</b></response>
+        /// <response code="404">`Not Found` Acesso a um <i>endpoint</i> que não existe. (Sem corpo de resposta)</response>
+        [ProducesResponseType(typeof(ResponseMessage), StatusCodes.Status200OK)]
         [HttpGet]
         public IActionResult GetStatus()
         {
