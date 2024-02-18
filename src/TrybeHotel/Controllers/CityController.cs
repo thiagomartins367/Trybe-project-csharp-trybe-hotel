cbc49f28811b4d6fe5ae1af6d4c6d034370aedd5
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-18 16:37:21 -0300

    docs(CityController.cs): 📝 Informações das rotas "GET /city", "POST /city" e "PUT /city"
    Informações adicionadas por comentário na documentação do "Swagger"

--- src/TrybeHotel/Controllers/CityController.cs
+++ src/TrybeHotel/Controllers/CityController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using TrybeHotel.Dto;
+using TrybeHotel.Errors;
 using TrybeHotel.Models;
 using TrybeHotel.Repository;
 
@@ -6,6 +8,7 @@ namespace TrybeHotel.Controllers
 {
     [ApiController]
     [Route("city")]
+    [Produces("application/json")]
     public class CityController : Controller
     {
         private readonly ICityRepository _repository;
@@ -15,6 +18,12 @@ namespace TrybeHotel.Controllers
             _repository = repository;
         }
 
+        /// <summary>
+        ///     Retorna todas as cidades cadastradas
+        /// </summary>
+        /// <response code="200">`OK` Retorna todas as cidades cadastradas</response>
+        /// <response code="404">`Not Found` Acesso a um <i>endpoint</i> que não existe. (Sem corpo de resposta)</response>
+        [ProducesResponseType(typeof(IEnumerable<CityDto>), StatusCodes.Status200OK)]
         [HttpGet]
         public IActionResult GetCities()
         {
@@ -22,6 +31,25 @@ namespace TrybeHotel.Controllers
             return Ok(cities);
         }
 
+        /// <summary>
+        ///     Adiciona uma nova cidade
+        /// </summary>
+        /// <remarks>
+        /// Request body example:
+        ///
+        ///     {
+        ///        "name": "Rio de Janeiro",
+        ///        "state": "RJ"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">`Created` Retorna os dados da nova cidade</response>
+        /// <response code="400">`Bad Request` Retorna resposta padrão de erro de validação de campos</response>
+        /// <response code="404">`Not Found` Acesso a um <i>endpoint</i> que não existe. (Sem corpo de resposta)</response>
+        /// <response code="415">`Unsupported Media Type` Retorna resposta padrão de tipo de mídia não suportado</response>
+        [ProducesResponseType(typeof(CityDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
         [HttpPost]
         public IActionResult PostCity([FromBody] City city)
         {
@@ -29,7 +57,27 @@ namespace TrybeHotel.Controllers
             return Created("", newCity);
         }
 
-        // 3. Desenvolva o endpoint PUT /city
+        /// <summary>
+        ///     Atualiza dados de uma cidade
+        /// </summary>
+        /// <remarks>
+        /// Request body example:
+        ///
+        ///     {
+        ///        "cityId": 1,
+        ///        "name": "Rio de Janeiro",
+        ///        "state": "RJ"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">`OK` Retorna os dados atualizados da cidade</response>
+        /// <response code="400">`Bad Request` Retorna resposta padrão de erro de validação de campos</response>
+        /// <response code="404">`Not Found` Retorna mensagem <b>"City not found"</b> <br /> <p>`Not Found` Acesso a um <i>endpoint</i> que não existe. (Sem corpo de resposta)</p></response>
+        /// <response code="415">`Unsupported Media Type` Retorna resposta padrão de tipo de mídia não suportado</response>
+        [ProducesResponseType(typeof(CityDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiErrorResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status415UnsupportedMediaType)]
         [HttpPut]
         public IActionResult PutCity([FromBody] City city)
         {
