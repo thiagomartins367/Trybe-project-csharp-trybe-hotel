e09eeee30dc592493da05ecb31f4900c68c85752
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-22 17:50:08 -0300

    feat(ConflictException.cs): Classe "ConflictException"
    Responsável por construir exceção de erro "Conflict" da API

---
+++ src/TrybeHotel/Errors/ApiExceptions/ConflictException.cs
@@ -0,0 +1,8 @@
+namespace TrybeHotel.Errors.ApiExceptions;
+
+public class ConflictException : Exception
+{
+    public ConflictException() : base() { }
+    public ConflictException(string? message) : base(message) { }
+    public ConflictException(string? message, Exception innerException) : base(message, innerException) { }
+}
