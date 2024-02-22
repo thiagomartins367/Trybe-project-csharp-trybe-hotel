ee378004edf406f13327adf5b2b37052cda6b61e
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-22 17:36:56 -0300

    refactor(HotelController.cs): :recycle: Importações não utilizadas removidas

--- src/TrybeHotel/Controllers/HotelController.cs
+++ src/TrybeHotel/Controllers/HotelController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using TrybeHotel.Models;
 using TrybeHotel.Repository;
 using TrybeHotel.Dto;
-using TrybeHotel.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using TrybeHotel.Errors;
