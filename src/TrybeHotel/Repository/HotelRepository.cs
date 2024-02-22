69693d78eb50bfd244b4c82ab2571a161d0d800e
THIAGO MARTINS <thiago17thiago@gmail.com>
2024-02-22 17:55:28 -0300

    refactor(HotelRepository.cs): Comentários desnecessários removidos

--- src/TrybeHotel/Repository/HotelRepository.cs
+++ src/TrybeHotel/Repository/HotelRepository.cs
@@ -7,12 +7,12 @@ namespace TrybeHotel.Repository
     public class HotelRepository : IHotelRepository
     {
         protected readonly ITrybeHotelContext _context;
+
         public HotelRepository(ITrybeHotelContext context)
         {
             _context = context;
         }
 
-        //  5. Refatore o endpoint GET /hotel
         public IEnumerable<HotelDto> GetHotels()
         {
             return _context.Hotels.Select(hotel => new HotelDto()
@@ -26,7 +26,6 @@ namespace TrybeHotel.Repository
             });
         }
 
-        // 6. Refatore o endpoint POST /hotel
         public HotelDto AddHotel(Hotel hotel)
         {
             _context.Hotels.Add(hotel);
